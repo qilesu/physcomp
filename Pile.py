d2a3d6f7c842db5d314ef84c971a382fac2b2a5f
David Su <qilesu@g.ucla.edu>
2018-11-10 10:44:22 -0800

    Added save and load feature

--- Pile.py
+++ Pile.py
@@ -1,46 +1,81 @@
 import numpy as np
 
 class Pile: 
-	def __init__(self, Lx, initialH, dx, dy, initialT):
-		initialO2 = 0.272 #kg m-3 simple paper
-		initialX = 2 #mol/m3
-		self.meshTemp = np.full((round(Lx/dx), round(initialH/dy)), initialT, dtype='float64') # initial temperature in C
-		self.meshO2 = np.full((round(Lx/dx), round(initialH/dy)), initialO2+0.01, dtype='float64') 
-		self.meshX = np.full((round(Lx/dx), round(initialH/dy)), initialX, dtype='float64')
-		self.setBoundaryCondition(self.meshTemp, 273+10, 273+15, 273+10, 273+10)
-		self.setBoundaryCondition(self.meshO2, initialO2, initialO2, initialO2, initialO2)
-		self.dx = dx
-		self.dy = dy
-		self.rhoMin = 100 #kg m-3
-		self.resistance = 73/9.8 # E/g
-		self.dryFraction = 0.3 #mass fraction
-		self.area = 1
-		self.height = initialH
-		self.initMass()
-		self.averageRho()
-		self.computeVoidFraction() #volume fraction
+        def __init__(self, Lx, initialH, dx, dy, initialT):
+                initialO2 = 0.272 #kg m-3 simple paper
+                initialX = 2 #mol/m3
+                self.meshTemp = np.full((round(Lx/dx), round(initialH/dy)), initialT, dtype='float64') # initial temperature in C
+                self.meshO2 = np.full((round(Lx/dx), round(initialH/dy)), initialO2+0.01, dtype='float64') 
+                self.meshX = np.full((round(Lx/dx), round(initialH/dy)), initialX, dtype='float64')
+                self.setBoundaryCondition(self.meshTemp, 273+10, 273+15, 273+10, 273+10)
+                self.setBoundaryCondition(self.meshO2, initialO2, initialO2, initialO2, initialO2)
+                self.dx = dx
+                self.dy = dy
+                self.rhoMin = 100 #kg m-3
+                self.resistance = 73/9.8 # E/g
+                self.dryFraction = 0.3 #mass fraction
+                self.area = 1
+                self.height = initialH
+                self.initMass()
+                self.averageRho()
+                self.computeVoidFraction() #volume fraction
 
-	def initMass(self):
-		self.mass = self.area*self.rhoMin*(np.exp(self.height/(self.resistance*self.dryFraction))-1)*self.resistance*self.dryFraction
+        def initMass(self):
+                self.mass = self.area*self.rhoMin*(np.exp(self.height/(self.resistance*self.dryFraction))-1)*self.resistance*self.dryFraction
 
-	def computeHeight(self):
-		self.height = self.dryFraction*self.resistance*np.log(1+self.mass/(self.area*self.rhoMin*self.resistance*self.dryFraction))
+        def computeHeight(self):
+                self.height = self.dryFraction*self.resistance*np.log(1+self.mass/(self.area*self.rhoMin*self.resistance*self.dryFraction))
 
-	def averageRho(self):
-		self.bulkRho = self.mass/(self.area*self.height)
+        def averageRho(self):
+                self.bulkRho = self.mass/(self.area*self.height)
 
-	def eatMass(self, decrement):
-		self.mass -= decrement
-		self.computeHeight()
-		self.averageRho()
+        def eatMass(self, decrement):
+                self.mass -= decrement
+                self.computeHeight()
+                self.averageRho()
 
-	def computeVoidFraction(self):
-		self.voidFraction = 1-(self.dryFraction/1150+1/1000-self.dryFraction/1000)*self.bulkRho
+        def computeVoidFraction(self):
+                self.voidFraction = 1-(self.dryFraction/1150+1/1000-self.dryFraction/1000)*self.bulkRho
 
-	def setBoundaryCondition(self, grid, top, bottom, left, right):
-		for j in range(1, len(grid[0])-1):
-			grid[0][j] = left # top edge
-			grid[len(grid)-1][j] = right # bottom edge
-		for i in range(1, len(grid)-1):
-			grid[i][0] = bottom # left edge
-			grid[i][len(grid[0])-1] = top # right edge
+        def setBoundaryCondition(self, grid, top, bottom, left, right):
+                for j in range(1, len(grid[0])-1):
+                        grid[0][j] = left # top edge
+                        grid[len(grid)-1][j] = right # bottom edge
+                for i in range(1, len(grid)-1):
+                        grid[i][0] = bottom # left edge
+                        grid[i][len(grid[0])-1] = top # right edge
+        def saveFields(self):
+                np.savetxt("meshTemp.dat", self.meshTemp)
+                np.savetxt("meshO2.dat", self.meshO2)
+                np.savetxt("meshX.dat", self.meshX)
+                # store loose variables
+                empty = np.array([])
+                headerString = str(self.dx) + "\n"
+                headerString += str(self.dy) + "\n"
+                headerString += str(self.rhoMin) + "\n"
+                headerString += str(self.resistance) + "\n"
+                headerString += str(self.dryFraction) + "\n"
+                headerString += str(self.area) + "\n"
+                headerString += str(self.height) + "\n"
+                headerString += str(self.mass) + "\n"
+                headerString += str(self.bulkRho) + "\n"
+                headerString += str(self.voidFraction)
+                np.savetxt("otherVariables.dat", empty, header=headerString, comments='')
+        def loadFields(self):
+                try:
+                        self.meshTemp = np.loadtxt("meshTemp.dat")
+                        self.meshO2 = np.loadtxt("meshO2.dat")
+                        self.meshX = np.loadtxt("meshX.dat")
+                        A = np.loadtxt("otherVariables.dat")
+                        self.dx = A[0]
+                        self.dy = A[1]
+                        self.rhoMin = A[2]
+                        self.resistance = A[3]
+                        self.dryFraction = A[4]
+                        self.area = A[5]
+                        self.height = A[6]
+                        self.mass = A[7]
+                        self.bulkRho = A[8]
+                        self.voidFraction = A[9]
+                except:
+                        print("ERROR OCCURED WHEN LOADING SAVE FILE.")
--- heatDiffusion.py
+++ heatDiffusion.py
@@ -8,30 +8,32 @@ import evolve
 from Pile import Pile
 
 
-	
+        
 def main():
-	Lx = 1 # length in x
-	Ly = 1 # length in y
-	dx = 0.05 # grid spacing m
-	dt = 5 # seconds
+        Lx = 1 # length in x
+        Ly = 1 # length in y
+        dx = 0.05 # grid spacing m
+        dt = 5 # seconds
 
-	#meshTemp = np.full((round(Lx/dx), round(Ly/dx)), 20, dtype='float64') # initial temperature in C
-	#setBoundaryCondition(meshTemp, 10, 15, 10, 10)
-	pile = Pile(Lx, Ly, dx, dx, 273+35)
-	steps = 1000
-	for i in range(steps):
-		evolve.timeEvolve(pile, dt)
+        #meshTemp = np.full((round(Lx/dx), round(Ly/dx)), 20, dtype='float64') # initial temperature in C
+        #setBoundaryCondition(meshTemp, 10, 15, 10, 10)
+        pile = Pile(Lx, Ly, dx, dx, 273+35)
+        pile.loadFields()
+        steps = 100
+        for i in range(steps):
+                evolve.timeEvolve(pile, dt)
 
-	print (pile.meshTemp)
-	plt.subplot(1, 3, 1)
-	plt.imshow(np.transpose(np.fliplr(pile.meshTemp)))
-	plt.subplot(1, 3, 2)
-	plt.imshow(np.transpose(np.fliplr(pile.meshO2)))
-	plt.subplot(1, 3, 3)
-	plt.imshow(np.transpose(np.fliplr(pile.meshX)))
-	plt.colorbar()
-	#fig = plt.figure(figsize=(244.0/72, 140.0/72))
-	plt.savefig('Diffusion-run.png', transparent=True)
+        print (pile.meshTemp)
+        plt.subplot(1, 3, 1)
+        plt.imshow(np.transpose(np.fliplr(pile.meshTemp)))
+        plt.subplot(1, 3, 2)
+        plt.imshow(np.transpose(np.fliplr(pile.meshO2)))
+        plt.subplot(1, 3, 3)
+        plt.imshow(np.transpose(np.fliplr(pile.meshX)))
+        plt.colorbar()
+        #fig = plt.figure(figsize=(244.0/72, 140.0/72))
+        plt.savefig('Diffusion-run.png', transparent=True)
+        pile.saveFields()
 
 if __name__ == '__main__':
-	sys.exit(main())
+        sys.exit(main())
